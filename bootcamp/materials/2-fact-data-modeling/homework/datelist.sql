ba128dab80c423513b47cd50a8eccdb4b42f9217
Syed Ahmad <SAHMAD@edge.gocolumbiamo.com>
2024-12-04 14:11:36 -0600

    hw2 q2

--- bootcamp/materials/2-fact-data-modeling/homework/datelist.sql
+++ bootcamp/materials/2-fact-data-modeling/homework/datelist.sql
@@ -2,43 +2,60 @@ DROP TABLE IF EXISTS  user_devices_cumulated;
 CREATE TABLE user_devices_cumulated(
 	user_id TEXT,
  	date_active DATE[],
- 	browser_type TEXT,
+ 	--browser_type TEXT,
  	date DATE,
- 	PRIMARY KEY(user_id)
+ 	PRIMARY KEY(user_id,date)
 );
-INSERT INTO user_devices_cumulated
-WITH yesterday AS (
-SELECT
-	*
-FROM user_devices_cumulated
-	WHERE date = DATE ('2022-12-31')
 
-),
+	  WITH 
+	   dedup AS (
+			SELECT *, ROW_NUMBER() OVER (PARTITION BY user_id,DATE(event_time)) as rn  FROM events
+		   WHERE user_id is not null
+			)
 
-today AS (
 
-SELECT 
-	CAST(user_id AS TEXT), 
-	DATE(event_time) as date_active
-FROM events
-	
-WHERE DATE(event_time) =  DATE ('2023-01-01') 
-	and user_id IS NOT NULL
-	GROUP BY 1,2
-)
+			SELECT * FROM dedup 
+			WHERE rn = 1;
 
-SELECT 
-COALESCE(t.user_id, y.user_id) AS user_id,
-CASE 
-	WHEN y.date_active IS NULL
-		THEN ARRAY[t.date_Active]
-	WHEN t.date_active IS NULL 
-		THEN y.date_active
-	ELSE y.date_Active || ARRAY [t.date_active]
-	
-END
-as dates_active,
-COALESCE (t.date_active, y.date + INTERVAL '1 day') as date
 
-FROM today t
-FULL OUTER JOIN yesterday y ON t.user_id = y.user_id
+		INSERT INTO user_devices_cumulated 
+	  WITH 
+	   dedup AS (
+			SELECT *, ROW_NUMBER() OVER (PARTITION BY user_id,DATE(event_time)) as rn  FROM events
+		     WHERE user_id IS NOT NULL
+			), 
+		deduped_events AS (
+
+			SELECT * FROM dedup 
+			WHERE rn = 1)
+	 ,yesterday AS (
+			SELECT
+				*
+			FROM user_devices_cumulated
+			WHERE date = DATE('2023-01-30')
+		),
+		today AS (
+			SELECT 
+				CAST(user_id AS TEXT), 
+				DATE(event_time) as date_active
+			FROM deduped_events
+			WHERE DATE(event_time) = DATE('2023-01-30') + INTERVAL '1 day'
+				AND user_id IS NOT NULL
+			GROUP BY 1,2
+		)
+		SELECT 
+			COALESCE(t.user_id, y.user_id) AS user_id,
+			CASE 
+				WHEN y.date_active IS NULL
+					THEN ARRAY[t.date_active]
+				WHEN t.date_active IS NULL 
+					THEN y.date_active
+				ELSE  ARRAY[t.date_active] || y.date_active 
+			END as date_active,
+			COALESCE(t.date_active, y.date + INTERVAL '1 day') as date
+		FROM today t
+		FULL OUTER JOIN yesterday y ON t.user_id = y.user_id
+		
+		
+		SELECT * FROM user_devices_cumulated where date = date('2023-01-31')
+
